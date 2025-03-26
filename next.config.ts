3322b13ae665b16423f8af80cc28598712a612ba
Louay <116605541+LouayCoding@users.noreply.github.com>
2025-03-26 05:33:40 +0100

    Update dependencies and project files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {},
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
